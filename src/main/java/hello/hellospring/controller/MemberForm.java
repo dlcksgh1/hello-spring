253b8ff3b002643bb0e2904ed00633a9d2beee73
Lee  Chanho <sma05091@nate.com>
2022-12-07 23:01:48 +0900

    회원 서비스 , 스프링 빈 등록 및 회원기능 등록, 조회

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
